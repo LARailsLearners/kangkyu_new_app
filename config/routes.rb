e3aba9665488df044eb16824bbb73d692ef6d3f5
kangkyu <kangkyu1111@gmail.com>
2015-06-01 18:43:29 -0700

    add login to navbar and refactor migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'welcome#index'
   get 'welcome/index'
 
-  resources :users
+  devise_for :users
+  # routes :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
