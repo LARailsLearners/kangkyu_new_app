c4df7f47456a3428a5fba379aa0fcf01a0119798
kangkyu <kangkyu1111@gmail.com>
2015-07-27 14:20:33 -0700

    rake db:migrate after setting db names on database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20150716225407) do
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
     t.string   "lastname"
+    t.string   "email",                  default: "", null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
